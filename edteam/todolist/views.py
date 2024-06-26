974d1079525b9e11c0a7978ad3cf78b750ec2b87
DaniloTrocel <158530079+DaniloTrocel@users.noreply.github.com>
2024-06-26 19:49:01 -0400

    Creando y probando los metodos post y get

--- edteam/todolist/views.py
+++ edteam/todolist/views.py
@@ -1,6 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from .models import Tarea
+from .serializers import TareaSerializer
+
 class IndexView(APIView):
     def get(self, request):
         context = {
@@ -8,3 +11,17 @@ class IndexView(APIView):
             'content': 'servidor activo'
         }
         return Response(context)   
+    
+class TareaView(APIView):
+    def get(self, request):
+        data = Tarea.objects.all()
+        ser_data = TareaSerializer(data, many=True)
+        return Response(ser_data.data)
+    
+    def post(self, request):
+        ser_data = TareaSerializer(data=request.data)
+        ser_data.is_valid(raise_exception=True)
+        ser_data.save()
+        
+        return Response(ser_data.data)
+        
