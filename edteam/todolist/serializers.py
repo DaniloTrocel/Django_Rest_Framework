400dac5b49967a6e868fc9220b096a41a84d51af
DaniloTrocel <158530079+DaniloTrocel@users.noreply.github.com>
2024-06-26 21:08:30 -0400

    Dando forma de json con to_representation

--- edteam/todolist/serializers.py
+++ edteam/todolist/serializers.py
@@ -4,4 +4,13 @@ from .models import Tarea
 class TareaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tarea
-        fields = '__all__'        
+        fields = '__all__'      
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        if instance.estado == "1":
+            estado = "Pendiente"
+        elif instance.estado == "2":
+            estado = "Terminado"    
+        representation['estado'] = estado
+        return(representation)      
