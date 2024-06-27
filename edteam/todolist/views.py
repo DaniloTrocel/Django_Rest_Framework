4f2b46be755db6e9fd80261de624dddc221e1c3e
DaniloTrocel <158530079+DaniloTrocel@users.noreply.github.com>
2024-06-26 20:40:18 -0400

    Creando los metodos put y delete

--- edteam/todolist/views.py
+++ edteam/todolist/views.py
@@ -1,5 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import Http404
+from rest_framework import status
 
 from .models import Tarea
 from .serializers import TareaSerializer
@@ -22,6 +24,33 @@ class TareaView(APIView):
         ser_data = TareaSerializer(data=request.data)
         ser_data.is_valid(raise_exception=True)
         ser_data.save()
-        
+
         return Response(ser_data.data)
+    
+class TareaDetailView(APIView):
+    def get_object(self, pk):
+        try:
+            return Tarea.objects.get(pk=pk)
+        except Tarea.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        data = self.get_object(pk)
+        ser_data = TareaSerializer(data)
+        return Response(ser_data.data)       
+    
+    def put(self, request, pk):
+        data = self.get_object(pk)
+        ser_data = TareaSerializer(data, data=request.data)
+
+        if ser_data.is_valid():
+            ser_data.save()
+            return Response(ser_data.data)
+
+        return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        data = self.get_object(pk)
+        data.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
         
